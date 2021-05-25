df99ff040f5ca22e127558a598b5cff88388eb4b
mishra-s <70597734+mishra-s@users.noreply.github.com>
2021-05-25 16:23:06 +0530

    Update doraMangoTree.java

--- doraMangoTree.java
+++ doraMangoTree.java
@@ -1,7 +1,7 @@
 package com.company;
 import java.util.*;
 
-public class doraMangoTree {
+public class doraGarden {
 
 
    static int checkMangoTree( int treeNumber, int rowNumber, int columnNumber )
